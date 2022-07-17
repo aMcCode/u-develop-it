a9978f0b6c94f8b3577b8a5711bdbd13b5fca436
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-07-17 00:09:55 -0400

    created and seeded parties table

--- server.js
+++ server.js
@@ -107,8 +107,8 @@ app.post('/api/candidate', ({ body }, res) => {
   // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
-  });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });
+});
